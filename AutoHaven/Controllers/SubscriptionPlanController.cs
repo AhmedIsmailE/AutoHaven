430e797bab30aca9cb15470b3e90486e437314a3
Helios165 <ahmed.ismail.elshazly.165@gmail.com>
2025-11-26 21:21:51 +0200

    homepageandsomecomments

---
+++ AutoHaven/Controllers/SubscriptionPlanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AutoHaven.Controllers
+{
+    public class SubscriptionPlanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
